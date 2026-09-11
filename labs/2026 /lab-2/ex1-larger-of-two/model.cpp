31f3086e429efcc3537e832b8f71aa307fe3cd76
muthintimo-afk <muthintimo@gmail.com>
2026-09-11 12:26:15 +0200

    Update model.cpp

--- labs/2026 /lab-2/ex1-larger-of-two/model.cpp
+++ labs/2026 /lab-2/ex1-larger-of-two/model.cpp
@@ -1,1 +1,16 @@
 //Lab 2 Ex1: placeholder
+#include <iostream>
+using namespace std;
+int main() {
+  int a=34;
+  int b=17;
+  int larger;
+if(a>b){
+larger=a;
+}else{
+larger=b;
+}
+cout<<"Larger: "<<larger<<endl;
+return 0;
+}
+
